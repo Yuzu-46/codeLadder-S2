29b875d4c17dd520b88e81090592a56918138b94
柚子46 <3942346547@qq.com>
2026-02-01 17:09:46 +0800

    feat(server): 添加可交互对象管理器
    - 引入 InteractableMgr 模块用于管理可交互对象
    - 在 App 类中添加 _interactableMgr 私有属性并初始化实例
    - 修改 start 方法参数类型从 number | string 改为 string
    - 在应用程序启动时调用 _interactableMgr.start() 方法

--- server/src/App.ts
+++ server/src/App.ts
@@ -3,6 +3,7 @@ import { Singleton } from './framework/common/Singleton';
 import { PlayerMgr } from './play/mgr/PlayerMgr';
 import { PlayerDataMgr } from './play/mgr/PlayerDataMgr';
 import { RemoteMgr } from './play/mgr/RemoteMgr';
+import { InteractableMgr } from './play/mgr/InteractableMgr';
 
 export class App extends Singleton<App>() {
     /** 玩家管理器 / Player manager */
@@ -11,17 +12,20 @@ export class App extends Singleton<App>() {
     private _playerDataMgr: PlayerDataMgr = PlayerDataMgr.instance;
     /** 远程管理器 / Remote manager */
     private _remoteMgr: RemoteMgr = RemoteMgr.instance;
+    /** 可交互对象管理器 / Interactable manager */
+    private _interactableMgr: InteractableMgr = InteractableMgr.instance;
 
     /**
      * 应用程序启动方法 / Application start method
      * @param id 地图ID / Map ID
      */
-    public start(id: number | string = ''): void {
+    public start(id: string = ''): void {
         console.log('(server) Hello World!');
         // 由于服务端中用户群体广泛，来源多样，建议在每个 t 函数中显式传入当前用户的语言标识，以确保多语言内容能够正确匹配用户所需的语言版本。
         console.log('(server)：', i18n.t('welcome_game', { lng: 'zh-CN' }));
         console.log('(server)：', i18n.t('welcome_ap', { lng: 'en' }));
         this._playerMgr.start();
         this._remoteMgr.start();
+        this._interactableMgr.start(id);
     }
 }
