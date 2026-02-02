4d3e3fe1648454982d5afb3ffa15a52ea2149773
柚子46 <3942346547@qq.com>
2026-02-03 05:52:55 +0800

    feat(types): 同步游戏资产资源定义
    将GameAssets.d.ts中的模型资源数量从4个更新为41个，
    新增了包括通用火焰动画、各种生物模型、家具、厨房用品等资源定义。

--- client/UiIndex/screens/UiIndex_screen.ts
+++ client/UiIndex/screens/UiIndex_screen.ts
@@ -1,6 +1,6 @@
 // 本文件由ArenaPro生成的UI索引工具，UiIndex文件夹均为自动生成，请勿手动修改
 // 屏幕: screen
-// 生成时间: 2026年2月1日星期日 GMT+8 16:06:27
+// 生成时间: 2026年2月3日星期二 GMT+8 05:47:46
 
 import ClientUIWindow from '../ClientUIWindow';
 
