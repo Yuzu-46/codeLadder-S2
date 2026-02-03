7af525f46e1255881baa2013110dd9b432b34085
柚子46 <3942346547@qq.com>
2026-02-03 20:18:48 +0800

    feat(GameAssets): 更新游戏资源定义并重新生成UI索引
    更新了GameAssets.d.ts中的模型资源配置，新增了"mesh/巨人屋-灶台.vb"资源，
    使模型资源总数从41个增加到42个。同时重新生成了UI索引文件以保持代码同步。

--- client/UiIndex/screens/UiIndex_screen.ts
+++ client/UiIndex/screens/UiIndex_screen.ts
@@ -1,6 +1,6 @@
 // 本文件由ArenaPro生成的UI索引工具，UiIndex文件夹均为自动生成，请勿手动修改
 // 屏幕: screen
-// 生成时间: 2026年2月3日星期二 GMT+8 05:47:46
+// 生成时间: 2026年2月3日星期二 GMT+8 20:18:04
 
 import ClientUIWindow from '../ClientUIWindow';
 
