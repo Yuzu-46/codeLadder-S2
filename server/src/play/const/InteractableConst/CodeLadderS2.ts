cda58f253f325d6145f9eb91881448bed25cdec0
柚子46 <3942346547@qq.com>
2026-02-03 21:36:51 +0800

    feat(CodeLadderS2): 添加灶台位置配置和交互对象
    新增灶台位置数组配置，包含四个灶台的坐标位置
    添加灶台交互对象数据，包括模型、位置、缩放和旋转设置
    为现有桌子交互对象添加交互半径属性

--- server/src/play/const/InteractableConst/CodeLadderS2.ts
+++ server/src/play/const/InteractableConst/CodeLadderS2.ts
@@ -13,6 +13,18 @@ const tablePositions: { x: number; z: number }[] = [
 ];
 
 /**
+ * 灶台位置 / Stove positions
+ * @property x - x坐标 / x coordinate
+ * @property z - z坐标 / z coordinate
+ */
+const stovePositions: { x: number; z: number }[] = [
+    { x: 35, z: 17 },
+    { x: 37, z: 17 },
+    { x: 39, z: 17 },
+    { x: 41, z: 17 },
+];
+
+/**
  * 天梯S2 定制可交互对象数据 / CodeLadderS2 Custom Interactable Data
  */
 const codeLadderS2: IInteractableData[] = [
@@ -24,7 +36,19 @@ const codeLadderS2: IInteractableData[] = [
             position: new GameVector3(position.x, 1.5, position.z),
             meshScale: new GameVector3(0.055, 0.1, 0.154),
         },
-        interactHint: '',
+        interactRadius: 2,
+    })),
+    ...stovePositions.map<IInteractableData>((position) => ({
+        id: `stove_${position.x}_${position.z}`,
+        token: InteractableType.StoveProp,
+        entityConfig: {
+            mesh: 'mesh/巨人屋-灶台.vb',
+            position: new GameVector3(position.x, 1.8, position.z),
+            meshScale: new GameVector3(0.037, 0.037, 0.037),
+            meshOrientation: new GameQuaternion(0, 0, 0, 1).rotateY(
+                Math.PI / 2
+            ),
+        },
         interactRadius: 2,
     })),
 ];
