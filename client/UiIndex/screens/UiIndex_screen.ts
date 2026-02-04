2d6c84ab3883143314373dff9c3246f5e2c94888
柚子46 <3942346547@qq.com>
2026-02-04 15:36:53 +0800

    chore(ui-index): 更新UI索引文件生成时间
    更新ClientUIWindow.ts、index.ts、screens/UiIndex_screen.ts和GameAssets.d.ts
    中的生成时间戳从2026年2月3日更新为2026年2月4日
    
    更新GameAssets.d.ts中的模型资源统计从42个资源更新为44个资源，
    并添加了新的模型资源包括mesh/灶台.vb和mesh/桌子1.vb

--- client/UiIndex/screens/UiIndex_screen.ts
+++ client/UiIndex/screens/UiIndex_screen.ts
@@ -1,6 +1,6 @@
 // 本文件由ArenaPro生成的UI索引工具，UiIndex文件夹均为自动生成，请勿手动修改
 // 屏幕: screen
-// 生成时间: 2026年2月3日星期二 GMT+8 20:18:04
+// 生成时间: 2026年2月4日星期三 GMT+8 14:39:10
 
 import ClientUIWindow from '../ClientUIWindow';
 
