f94b3aa3b77775f3db223399e4cdc5c25ed3f4f1
柚子46 <3942346547@qq.com>
2026-02-05 17:31:45 +0800

    feat(movableProp): 添加可移动道具穿戴功能
    为BaseMovableProp基类添加道具类型属性和wear方法，
    实现道具穿戴到玩家身上的功能。同时更新PlateProp类，
    添加children属性和交互逻辑，支持玩家携带盘子道具
    以及处理玩家与盘子的交互事件。

--- server/src/play/entity/interactable/prop/movableProp/base/BaseMovableProp.ts
+++ server/src/play/entity/interactable/prop/movableProp/base/BaseMovableProp.ts
@@ -1,11 +1,37 @@
 import { Interactable } from '../../../base/Interactable';
 import type { IInteractableData } from '../../../../../data/InteractableData';
+import { MovablePropConfig } from '../../../../../config/MovablePropConfig';
+import type { InGamePlayer } from '../../../../player/GamePlayer';
+import type { ContainerType } from '../../../../../const/ContainerConst';
+import type { FoodType } from '../../../../../const/FoodConst';
+import { InteractableMgr } from '../../../../../mgr/InteractableMgr';
 
 /**
  * 可移动道具基类 / Movable prop base class
  */
 export abstract class BaseMovableProp extends Interactable {
+    /**
+     * 道具类型 / Prop type
+     */
+    protected _type: ContainerType | FoodType | null = null;
+
     public start(config: IInteractableData): void {
         super.start(config);
+    }
+
+    /**
+     * 穿戴到玩家上
+     * @param player 玩家对象 / Player object
+     */
+    public wear(player: InGamePlayer): void {
+        if (this._type) {
+            player.entity?.player.addWearable({
+                bodyPart: GameBodyPart.TORSO,
+                mesh: MovablePropConfig.data.plate.mesh,
+                ...MovablePropConfig.data.plate.wearableConfig,
+            });
+
+            InteractableMgr.instance.destroyInteractable(this.id);
+        }
     }
 }
--- server/src/play/entity/interactable/prop/movableProp/container/PlateProp.ts
+++ server/src/play/entity/interactable/prop/movableProp/container/PlateProp.ts
@@ -3,14 +3,44 @@ import { FactoryToken } from '@src/framework/common/factory/AbstractFactory';
 import { InteractableType } from '../../../../../const/TokenConst';
 import { ContainerState } from '../../../../../const/ContainerConst';
 import type { IInteractableData } from '../../../../../data/InteractableData';
+import { PlayerMgr } from '../../../../../mgr/PlayerMgr';
+import type { InGamePlayer } from '../../../../player/GamePlayer';
 
 /**
  * 盘子道具 / Plate prop
  */
 @FactoryToken(InteractableType.PlateProp)
 export class PlateProp extends BaseMovableProp {
+    /**
+     * 子项
+     */
+    public children: BaseMovableProp | null = null;
+
     public start(config: IInteractableData): void {
         super.start(config);
         console.log('(Server) PlateProp start with id ', config.id);
+        this._type = 'plate';
+    }
+
+    public async onInteract(event: GameInteractEvent): Promise<void> {
+        super.onInteract(event);
+        const { entity } = event;
+        const player = PlayerMgr.instance.getPlayer(
+            entity.player.userId
+        ) as InGamePlayer;
+        if (player.carryingProp) {
+            // 如果玩家拿着某道具
+            // more...
+        } else {
+            // 玩家没有拿着道具
+            player.carryingProp = {
+                data: {
+                    type: 'plate',
+                    state: ContainerState.CLEAN,
+                },
+            };
+            this.children?.wear(player);
+            this.wear(player);
+        }
     }
 }
