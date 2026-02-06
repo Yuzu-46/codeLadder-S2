79aea458eb830a2a898e70e89392c0ac0dfbdadd
柚子46 <3942346547@qq.com>
2026-02-06 23:05:13 +0800

    feat(game): 食材箱道具添加交互功能
    - 添加食材类型属性用于标识食材种类
    - 实现玩家交互时自动拾取对应食材功能
    - 增加FoodBoxProp配置接口类型定义
    - 引入食材状态和类型常量支持

--- server/src/play/entity/interactable/prop/immovableProp/FoodBoxProp.ts
+++ server/src/play/entity/interactable/prop/immovableProp/FoodBoxProp.ts
@@ -1,15 +1,46 @@
 import { BaseImmovableProp } from './BaseImmovableProp';
 import { FactoryToken } from '@src/framework/common/factory/AbstractFactory';
 import { InteractableType } from '../../../../const/TokenConst';
-import type { IInteractableData } from '../../../../data/InteractableData';
+import type {
+    IFoodBoxPropConfig,
+    IInteractableData,
+} from '../../../../data/InteractableData';
+import { PlayerMgr } from '../../../../mgr/PlayerMgr';
+import { InGamePlayer } from '../../../player/GamePlayer';
+import {
+    IngredientState,
+    type IngredientType,
+} from '../../../../const/FoodConst';
 
 /**
  * 无限食材箱 / Food Box Prop
  */
 @FactoryToken(InteractableType.FoodBoxProp)
 export class FoodBoxProp extends BaseImmovableProp {
-    public start(config: IInteractableData): void {
+    /**
+     * 食材类型 / Food type
+     */
+    public type: IngredientType | null = null;
+
+    public start(config: IFoodBoxPropConfig): void {
         super.start(config);
+        this.type = config.foodType;
         console.log('(Server) FoodBoxProp start with id ', config.id);
+    }
+
+    public async onInteract(event: GameInteractEvent): Promise<void> {
+        super.onInteract(event);
+        const player = PlayerMgr.instance.getPlayer(event.entity.player.userId);
+        if (player && player instanceof InGamePlayer && this.type) {
+            player.pickUpProp({
+                container: null,
+                foods: [
+                    {
+                        type: this.type,
+                        state: IngredientState.RAW,
+                    },
+                ],
+            });
+        }
     }
 }
