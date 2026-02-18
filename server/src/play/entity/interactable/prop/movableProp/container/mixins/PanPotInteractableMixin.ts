5f396efe8c9a147aa91139c93ca976576fdf09d8
柚子46 <3942346547@qq.com>
2026-02-18 15:36:25 +0800

    feat(play): 添加锅具交互Mixin以支持食物交互逻辑

---
+++ server/src/play/entity/interactable/prop/movableProp/container/mixins/PanPotInteractableMixin.ts
@@ -0,0 +1,68 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import type { InGamePlayer } from '../../../../../player/GamePlayer';
+import { ConfigMgr } from '../../../../../../mgr/ConfigMgr';
+import type { BaseContainerProp } from '../../base/BaseContainerProp';
+
+/**
+ * 锅具交互Mixin (Pot/Pan Interactable Mixin)
+ * @param Base - 基类 / Base class
+ * @returns 混入后的类 / Mixin class
+ */
+export function PanPotInteractableMixin<
+    TBase extends new (...args: any[]) => BaseContainerProp,
+>(Base: TBase): TBase {
+    return class extends Base {
+        protected onInteractCarryingContainerProp(player: InGamePlayer): void {
+            if (this.food?.canWear(player.carryingProp)) {
+                this.food.wear(player);
+            }
+        }
+
+        protected onInteractCarryingFoodProp(player: InGamePlayer): void {
+            // 如果不能容纳食物直接忽略
+            if (
+                !this._fsm ||
+                !ConfigMgr.instance.getContainerConfig(
+                    this._type!,
+                    this._fsm.State
+                )?.canHoldFood
+            ) {
+                return;
+            }
+
+            // 判断是否可以烹饪
+            const ingredients = [
+                ...(this.food?.data || []),
+                ...player.carryingProp.foods,
+            ];
+            if (
+                ingredients.length === 1 &&
+                !ConfigMgr.instance.getIngredientConfig(
+                    ingredients[0].type,
+                    ingredients[0].state
+                )?.canBeCooked
+            ) {
+                return;
+            }
+
+            // 检测食物是否可以放置
+            if (this.food && !this.food.canWear(player.carryingProp)) {
+                // 这里使用canWear方法判断是因为逻辑一致
+                return;
+            }
+
+            if (this.entity) {
+                player.placeProp(this.entity?.position, this);
+            }
+        }
+
+        protected onInteractNotCarryingProp(player: InGamePlayer): void {
+            if (this.food?.canWear(player.carryingProp) || !this.food) {
+                // 如果有绑定的食物/食材，那么也穿戴到玩家身上
+                this.food?.wear(player);
+
+                this.wear(player);
+            }
+        }
+    };
+}
