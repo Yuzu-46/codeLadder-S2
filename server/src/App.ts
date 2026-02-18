676ce1432c26a284a2fd9cd8b5636bbb073a392c
柚子46 <3942346547@qq.com>
2026-02-18 09:57:59 +0800

    feat(server): 添加配置管理器并初始化
    - 引入 ConfigMgr 模块用于管理游戏配置
    - 在 App 类中添加配置管理器实例
    - 在应用程序启动时加载配置数据

--- server/src/App.ts
+++ server/src/App.ts
@@ -5,6 +5,7 @@ import { PlayerDataMgr } from './play/mgr/PlayerDataMgr';
 import { RemoteMgr } from './play/mgr/RemoteMgr';
 import { InteractableMgr } from './play/mgr/InteractableMgr';
 import { SceneMgr } from './play/mgr/SceneMgr';
+import { ConfigMgr } from './play/mgr/ConfigMgr';
 
 export class App extends Singleton<App>() {
     /** 玩家管理器 / Player manager */
@@ -17,6 +18,8 @@ export class App extends Singleton<App>() {
     private _interactableMgr: InteractableMgr = InteractableMgr.instance;
     /** 场景管理器 / Scene manager */
     private _sceneMgr: SceneMgr = SceneMgr.instance;
+    /** 配置管理器 / Config manager */
+    private _configMgr: ConfigMgr = ConfigMgr.instance;
 
     /**
      * 应用程序启动方法 / Application start method
@@ -27,6 +30,7 @@ export class App extends Singleton<App>() {
         // 由于服务端中用户群体广泛，来源多样，建议在每个 t 函数中显式传入当前用户的语言标识，以确保多语言内容能够正确匹配用户所需的语言版本。
         console.log('(server)：', i18n.t('welcome_game', { lng: 'zh-CN' }));
         console.log('(server)：', i18n.t('welcome_ap', { lng: 'en' }));
+        this._configMgr.load();
         this._playerMgr.start();
         this._remoteMgr.start();
         this._interactableMgr.start(id);
