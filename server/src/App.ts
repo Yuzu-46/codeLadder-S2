661d1669544a14025e0c0962c76ed66f0d515231
柚子46 <3942346547@qq.com>
2026-02-20 17:30:08 +0800

    fix(App): 修正控制台日志中的大小写和国际化调用
    - 将 '(server)' 统一修正为 '(Server)' 以保持一致性
    - 修正国际化函数调用中的日志前缀格式

--- server/src/App.ts
+++ server/src/App.ts
@@ -26,10 +26,10 @@ export class App extends Singleton<App>() {
      * @param id 地图ID / Map ID
      */
     public start(id: string = ''): void {
-        console.log('(server) Hello World!');
+        console.log('(Server) Hello World!');
         // 由于服务端中用户群体广泛，来源多样，建议在每个 t 函数中显式传入当前用户的语言标识，以确保多语言内容能够正确匹配用户所需的语言版本。
-        console.log('(server)：', i18n.t('welcome_game', { lng: 'zh-CN' }));
-        console.log('(server)：', i18n.t('welcome_ap', { lng: 'en' }));
+        console.log('(Server)：', i18n.t('welcome_game', { lng: 'zh-CN' }));
+        console.log('(Server)：', i18n.t('welcome_ap', { lng: 'en' }));
         this._configMgr.load();
         this._playerMgr.start();
         this._remoteMgr.start();
